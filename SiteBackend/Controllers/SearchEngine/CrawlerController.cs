4d53cca60304df759dd9d8f9c8fbaf822b2441e7
zachary <syntheticoracle@pm.me>
2025-10-22 23:43:49 -0400

    ADD: CrawlerController.cs for Crawler API

--- SiteBackend/Controllers/SearchEngine/CrawlerController.cs
+++ SiteBackend/Controllers/SearchEngine/CrawlerController.cs
@@ -1,6 +1,35 @@
-namespace SiteBackend.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using SiteBackend.Models.SearchEngine.Index;
+using SiteBackend.Services;
 
-public class CrawlerController
+namespace SiteBackend.Controllers.SearchEngine;
+
+[ApiController]
+[Route("crawler")]
+public class CrawlerController : ControllerBase
 {
-    
+    private readonly ILogger<CrawlerController> _logger;
+    private readonly ICrawlerService  _crawlerService;
+
+    public CrawlerController(ILogger<CrawlerController> logger, ICrawlerService crawlerService)
+    {
+        _logger = logger;
+        _crawlerService = crawlerService;
+    }
+
+    [RequestSizeLimit(20 * (100 * 1024 * 1024))]
+    [HttpPost("scrape")]
+    public async Task<IActionResult> SubmitPages([FromBody] List<Page> pages)
+    {
+        await _crawlerService.BatchUpdatePagesAsync(pages);
+        
+        return Created("crawler", null);
+    }
+
+    [HttpGet("scrape")]
+    public async Task<IActionResult> Scrape()
+    {
+        var pages = await _crawlerService.GetEmptyPagesAsync();
+        return Ok(pages);
+    }
 }
