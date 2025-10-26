45a89d81b1d3d81418da4bc349988f7c95a9d61c
zachary <syntheticoracle@pm.me>
2025-10-26 11:25:11 -0400

    CHORE: AIService Docs on embeddings FIX: AIService embedding prompts

--- SiteBackend/Services/AIService.cs
+++ SiteBackend/Services/AIService.cs
@@ -16,7 +16,7 @@ public class AIService : IAIService
     }
 
     /// <summary>
-    /// Generates an embedding for a given prompt using the specified embedding model.
+    /// Generates an embedding for a given prompt using the dense embedding model.
     /// Meant for Page Submissions its just converting string to vector maps and computing vectored
     /// distances using <see cref="Pgvector"/>'s Cosine, Hamming, Jaccard and L distances.
     /// </summary>
@@ -25,11 +25,20 @@ public class AIService : IAIService
     /// <returns>Float array of embedding values.</returns>
     public async Task<float[]> GetEmbeddingAsync(string title, string text)
     {
-        var result = await _aiClient.GetDenseEmbeddingAsync(GetPromptEmbedding(title, text));
+        var result = await _aiClient.GetDenseEmbeddingAsync(GetDenseEmbedding(text));
         _logger.LogDebug($"Embedding result count: {result.Length}");
         return result;
     }
 
+    /// <summary>
+    ///     Use for generating vectors on a document for
+    ///     <see href="https://cohere.com/llmu/what-is-semantic-search">Semantic Search</see>
+    ///     ?this takes sentences/paragraphics and (normally)plots them to a hypersphere.?
+    ///     it uses <see href="https://milvus.io/docs/dense-vector.md">Dense Vectors</see> which increases storage costs
+    ///     but enables larger "semantic scope"
+    /// </summary>
+    /// <param name="wordChunks">[[Take, a, document], [break, it, into, paragraphs, and, split, the, words]]</param>
+    /// <returns></returns>
     public async Task<List<TextEmbedding>> EmbedDocumentAsync(string title, string[][] wordChunks)
     {
         _logger.LogDebug($"Embedding chunk count: {wordChunks.Length}");
@@ -46,19 +55,30 @@ public class AIService : IAIService
         return results;
     }
 
+    /// <summary>
+    ///     Use to get query vectors for a
+    ///     <see href="https://cohere.com/llmu/what-is-semantic-search">Semantic Search</see>
+    /// </summary>
+    /// <param name="query">Why did I choose a AI-powered search engine for a first project in a portfolio site?</param>
+    /// <returns>
+    ///     768 floating point vectors that represent the semantic meaning behind that query in the
+    ///     form of a pgVector type, it can be unpacked to <see cref="float">float</see>[]
+    /// </returns>
     public async Task<Vector> GetSearchVector(string query)
     {
-        return new Vector(await _aiClient.GetDenseEmbeddingAsync(GetPrompt("search result", query)));
+        return new Vector(await _aiClient.GetDenseEmbeddingAsync(GetDenseSearchPrompt(query)));
     }
 
-    string GetPromptEmbedding(string title, string text)
+    // TODO: figure out if granite-embedding wants a prompt
+    private string GetDenseEmbedding(string text)
     {
-        string prompt = GetPrompt($"{{title |{title}}}", $"text: {text}");
+        var prompt = $"{text}";
         return prompt;
     }
 
-    string GetPrompt(string task, string query)
+    // TODO: figure out if granite-embedding wants a prompt
+    private string GetDenseSearchPrompt(string query)
     {
-        return $"task: {task} | query: {query}";
+        return $"{query}";
     }
 }
