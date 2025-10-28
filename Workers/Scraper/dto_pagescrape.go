a28b9054fc3673af1b761c8a1d95da2654734e7e
zachary <syntheticoracle@pm.me>
2025-10-28 12:31:55 -0400

    ADD: Spiders

--- Workers/Scraper/dto_pagescrape.go
+++
@@ -1,6 +0,0 @@
-package main
-
-type DTOCrawlRequest struct {
-	PageID int    `json:"pageId"`
-	URL    string `json:"url"`
-}
--- Workers/Scraper/page_scraper.go
+++
@@ -1,7 +0,0 @@
-package main
-
-func scrapePage() {
-	browser, err = m.PW.Chromium.Launch(playwright.BrowserTypeLaunchOptions{
-		Args: []string{"--disable-blink-features=AutomationControlled"},
-	})
-}
