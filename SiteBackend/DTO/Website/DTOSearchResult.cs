cf971207ace099589ffa4ab0dad582a645579bda
zachary <syntheticoracle@pm.me>
2025-11-04 12:28:53 -0500

    FIX: Sending all page data in DTOSearchResult

--- SiteBackend/DTO/Website/DTOSearchResult.cs
+++ SiteBackend/DTO/Website/DTOSearchResult.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using SiteBackend.Models.SearchEngine.Index;
 
 namespace SiteBackend.DTO.Website;
@@ -8,18 +9,26 @@ public class DTOSearchResult
     {
     }
 
-    public DTOSearchResult(Page page, double resultScore, double denseDistance, double sparseDistance,
-        bool keywordMatch)
+    public DTOSearchResult(Page page, string pgTitle, string pgDesc, string pgUrl, double resultScore,
+        double denseDistance, double sparseDistance, bool keywordMatch)
     {
         ResultPage = page;
         ResultScore = resultScore;
         DenseDistance = denseDistance;
         SparseDistance = sparseDistance;
         KeywordMatch = keywordMatch;
+
+        ResultTitle = pgTitle;
+        ResultDescription = pgDesc;
+        ResultUrl = pgUrl;
     }
 
-    public Page ResultPage { get; set; }
 
+    [JsonIgnore] public Page ResultPage { get; set; }
+
+    public string ResultTitle { get; set; }
+    public string ResultUrl { get; set; }
+    public string ResultDescription { get; set; }
     public double ResultScore { get; set; }
     public double DenseDistance { get; set; }
     public double SparseDistance { get; set; }
